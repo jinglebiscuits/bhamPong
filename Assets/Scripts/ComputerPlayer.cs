9c5357d7fbf148e42efb7c836763094fff612d1d
AlienSync <lorenzokirkpatrick@gmail.com>
2015-02-04 21:24:08 -0600

    New Updates.
    Conflicts:
    Assets/Scenes/Main.unity

---
+++ Assets/Scripts/ComputerPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComputerPlayer : Player {
+
+	public GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if(inputPad.GetComponent<InputPad>().dragging)
+//		{
+//			paddle.GetComponent<Paddle>().Move(inputPad.GetComponent<InputPad>().target);
+//		}
+
+		paddle.GetComponent<Paddle>().Move(Camera.main.WorldToScreenPoint(ball.transform.position).x);
+	}
+}
